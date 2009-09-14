23e6cca6c13edf41682077cb55eadcf45dc37ba2
Vaclav Barta <vbar@comp.cz>
2009-09-14 08:56:24 +0200

    collecting node name distribution statistics

--- bin/sizetree.cc
+++ bin/sizetree.cc
@@ -6,7 +6,7 @@
 #include <libxml/parser.h>
 #include <iomanip>
 #include <iostream>
-#include <set>
+#include <map>
 #include <stdexcept>
 #include <string>
 
@@ -14,22 +14,28 @@
 // the nodes - Python's standard DOM doesn't even seem to have
 // ignorable whitespace, much less the same behavior as libxml2...
 
-TNodeIndex node_count = 0;
-TNodeIndex leaf_count = 0;
+typedef std::map<xmlNodePtr, TNodeIndex, less_xmlNodePtr> TOccMap;
 
-std::set<xmlNodePtr, less_xmlNodePtr> node_labels;
+TNodeIndex nodeCount = 0;
+TNodeIndex leafCount = 0;
+TOccMap nodeOcc;
 
 TNodeIndex do_size_tree(xmlNodePtr node, TNodeIndex depth)
 {
     TNodeIndex max_depth = ++depth;
 
-    ++node_count;
-    node_labels.insert(node);
+    ++nodeCount;
+
+    std::pair<TOccMap::iterator, bool> inres = nodeOcc.insert(
+        TOccMap::value_type(node, 1));
+    if (!inres.second) {
+	++(inres.first->second);
+    }
 
     xmlNodePtr ch = node->children;
     if (!ch)
     {
-	++leaf_count;
+	++leafCount;
     }
 
     while (ch)
@@ -50,10 +56,28 @@ void size_tree(const char *fname)
 {
     XDoc doc = xutil::parse_file(fname);
     TNodeIndex max_depth = do_size_tree(xutil::get_root_element(doc), 0);
-    std::cout << "tree size = " << node_count << std::endl;
-    std::cout << "label count = " << node_labels.size() << std::endl;
+
+    TNodeIndex mn = static_cast<TNodeIndex>(nodeOcc.size());
+    TNodeIndex mx = 0;
+    for (TOccMap::const_iterator i = nodeOcc.begin();
+	 i != nodeOcc.end();
+	 ++i) {
+        TNodeIndex occ = i->second;
+	if (occ < mn) {
+	    mn = occ;
+	}
+
+	if (occ > mx) {
+	    mx = occ;
+	}
+    }
+
+    std::cout << "tree size = " << nodeCount << std::endl;
+    std::cout << "label count = " << nodeOcc.size() << std::endl;
+    std::cout << "min label occurence = " << mn << std::endl;
+    std::cout << "max label occurence = " << mx << std::endl;
     std::cout << "max depth = " << max_depth << std::endl;
-    std::cout << "leaf count = " << leaf_count << std::endl;
+    std::cout << "leaf count = " << leafCount << std::endl;
 }
 
 int main(int argc, char **argv)
